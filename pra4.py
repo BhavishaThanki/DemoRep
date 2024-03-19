cf5a0985e0bcbfa0c95d31fe503f64cc2f4f4cc3
imbhavisha <imbhavisha66@gmail.com>
2024-03-19 14:04:18 +0530

    changes in pra4

--- pra4.py
+++ pra4.py
@@ -1,4 +1,5 @@
 # Create a lambda function that should double or multiply the number (that we will be passing in the lambda function) by 2. Store the lambda function in a variable named 'double_num'
 a=int(input("Enter a number: "))
+b=int(input("Enter second number : "))
 double_num=lambda a: 2*a
 print(double_num(a))
