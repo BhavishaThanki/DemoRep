652975eb711ed79d0690d6ee0a45b72c0d59a3d5
imbhavisha <imbhavisha66@gmail.com>
2024-03-19 12:23:24 +0530

    Changes in pra1

--- pra1.py
+++ pra1.py
@@ -1,7 +1,7 @@
 # Take three user inputs and print the greatest number from those inputs
 # using if-else condition. Edge cases, if any, should also be handled.
 
-a,b,c=map(int,input("Enter three space separated values a,b,c: ").split())
+a,b,c=map(int,input("Enter 3 space separated values a,b,c: ").split())
 if a==b==c:
     print("All values are same!!")
 elif a>=b and a>=c:
