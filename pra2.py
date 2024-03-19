d7f3409164f7e1a5a2e45c6ad9ee594a5e929e1e
imbhavisha <imbhavisha66@gmail.com>
2024-03-19 12:49:50 +0530

    added/modified files in branch

--- pra2.py
+++ pra2.py
@@ -2,8 +2,9 @@
 # of while loop fetch the even numbers and print the numbes
 
 lis=list((10, 23, 4, 26, 4, 75, 24, 54))
+lis2=list((1,3,5,7,9,13,15))
 lis_even=[]
-for i in lis:
+for i in lis2:
     if i%2==0:
         lis_even.append(i)
 print("Even values: ",lis_even)
