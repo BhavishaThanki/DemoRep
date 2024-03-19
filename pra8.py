34de733a7bd021dce39c7004f3bfeaf209e2fc3f
imbhavisha <imbhavisha66@gmail.com>
2024-03-19 15:02:18 +0530

    pra8 logic updated

--- pra8.py
+++ pra8.py
@@ -1,4 +1,6 @@
-#Adding test to check
+import pandas as pd 
+import numpy as np
 
-#logic 1
-#logic 2
+arr1= np.array([[12,33,7],[16,4,27]])
+print("Original array: \n",arr1)
+print("Sorted array: \n",np.sort(arr1))
